afb7d5e19c0379d851d7d3fc343db3f648348b24
hungrymonkey <tedchang2010@gmail.com>
2019-02-03 07:52:47 -0500

    added basic binary tree test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import BinaryTree from './data_structures/bin_tree';
+
+it("keep the array in order", () => {
+  var arr = [0, 1, 1, 2, 3, 4, 5, 6, 7]
+  var answer = [0, 1, 2, 3, 4, 5, 5, 6, 7];
+  var BST = BinaryTree.BinaryTree();
+  arr.forEach(function(val, idx) {
+    BST.insert(val, idx);
+  })
+  var n = BST.next();
+  var output = []
+  while(n) {
+    output.push(n.getValue());
+  }
+  expect(output, answer);
+});
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
